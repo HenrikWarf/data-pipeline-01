c843bf3758bb58ec95f3dfa5b7d66e53681443e2
Henrik Warfvinge <henrik@warfvinge.se>
2024-03-14 14:26:04 +0000

    added a subprocess again

--- pipeline.py
+++ pipeline.py
@@ -11,11 +11,16 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def run_pipeline():
 
+    print("Function Started")
+
     import subprocess
 
     result = subprocess.run(["/bin/sh", "script.sh"], shell=True, capture_output=True, text=True)
 
     print(result.stdout)
+
+    print("Function Completed")
+
      
 
 if __name__ == "__main__":
